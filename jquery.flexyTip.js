3935ce30438951bfee922e046e781f70fd948b76
Dan Beam <dan@danbeam.org>
2010-05-02 20:19:34 -0700

    Added some logging abilities, not sure why animation isn't working in some cases

--- jquery.flexyTip.js
+++ jquery.flexyTip.js
@@ -1,48 +1,66 @@
-/*
- * File: jquery.flexyTips.js
- * Version: 1.0.0 (Jan. 4, 2009)
+/* File        : jquery.flexyTips.js
+ * Version     : 1.0.0 (Jan. 4, 2009)
+ * Requires    : jQuery v1.2.6 or later
+ * Description : A flexible, un-obtrusive way to show a tool-tip almost anywhere
  * Copyright (C) 2009 Dan Beam
  * Licensed under the MIT License: http://www.opensource.org/licenses/mit-license.php
- * Requires: jQuery v1.2.6 or later
- * Description: A flexible, un-obtrusive way to show a tool-tip almost anywhere
  */
 
-;;;;;(function($) {
+; (function($) {
 
+    // allow debug messages when boolean is true and firebug is present
+    var debug = window.console && window.console.log || function(){};
+
+    // if there's something else named flexyTip on jQuery's prototype, sue me
     $.fn.flexyTip = function(html, settings) {
 
-        var defaultParams = {
+        debug("flexyTip started with the following settings:");
+
+        // merge the settings provided with these defaults
+        settings = $.extend({
             "xStart"        : "right",
             "yStart"        : "top",
             "direction"     : "none",
-            "tipCSS"        : {},
-            "containerCSS"  : { "opacity" : "0" },
-            "showAnimation" : { "opacity" : "1" },
-            "hideAnimation" : { "opacity" : "0" },
             "showDuration"  : "medium",
             "hideDuration"  : "medium",
             "showEasing"    : "swing",
             "hideEasing"    : "swing",
+            "tipCSS"        : {},
+            "containerCSS"  : { "opacity" : "0" },
+            "showAnimation" : { "opacity" : "1" },
+            "hideAnimation" : { "opacity" : "0" },
             "onBeforeShow"  : function() {},
             "onBeforeHide"  : function() {},
             "onAfterShow"   : function() {},
             "onAfterHide"   : function() {}
-        };
+        },
+        settings);
 
-        settings = $.extend(defaultParams, settings);
-
+        // iterate over all 
         return $(this).each(function() {
 
+            // these are global to both over and out hover functions
+            var tip, container;
+
+            // use jQuery's hover function for now, maybe switch to hoverIntent?
             $(this).hover(function() {
+                
+                debug("Starting onMouseOver part of flexyTip");
+                
+                // these are global to both over and out hover functions
+                container = document.createElement("span");
+                tip       = document.createElement("span");
 
-                var srcEl = $(this), offset = srcEl.offset(),
-                    xOffset = offset.left + ("right" == settings["xStart"] ? srcEl.outerWidth() : 0), yOffset = offset.top + ("bottom" == settings["yStart"] ? srcEl.outerHeight() : 0),
-                    direction = /left|right|up|down/.test(settings["direction"]) ? settings["direction"] : "none",
-                            
-                    container = document.createElement("span"),
-                    tip = document.createElement("span"),
+                // start really long var statement
 
-    
+                var srcEl     = $(this),
+                    offset    = srcEl.offset(),
+                    xOffset   = offset.left + ("right"  == settings["xStart"] ? srcEl.outerWidth()  : 0),
+                    yOffset   = offset.top  + ("bottom" == settings["yStart"] ? srcEl.outerHeight() : 0),
+
+                    tipWidth  = $(tip).outerWidth(),
+                    tipHeight = $(tip).outerHeight(),
+
                     containerCSS = {
                         "display"  : "block",
                         "position" : "absolute",
@@ -64,80 +82,92 @@
                         "none"  : {}
                     },
     
-                    sideBinding = positionBinding[settings["direction"]];
+                    direction   = /left|right|up|down/.test(settings["direction"]) ? settings["direction"] : "none",
+                    sideBinding = positionBinding[direction],
 
-                tipCSS = $.extend(tipCSS, sideBinding, settings["tipCSS"]);
+                    // this is extended later
+                    animation = {},
+
+                    // this is overwritten later
+                    startDimensions;
+
+                // extend default CSS
+                tipCSS       = $.extend(tipCSS, sideBinding, settings["tipCSS"]);
                 containerCSS = $.extend(containerCSS, settings["containerCSS"]);
 
-                // add the CSS to the cotainer
-                // and set the id to flexyLink
-                $(container).css(containerCSS).attr("id", "flexyTip");
+                // add the CSS to the cotainer and add the ID
+                $(container).css(containerCSS);
 
-                // add the CSS to the pop and
-                // populate with HTML
+                // add the CSS to the pop and populate with HTML
                 $(tip).css(tipCSS).html(html);
 
+                // there's a container and an inner element, here we're inserting the
+                // inner into the container (it doesn't live in the page DOM yet though)
                 container.appendChild(tip);
-
-                // call onShowStart as we insert into DOM
-                settings.onBeforeShow($('body')[0].appendChild(container));
-
-                var    tipWidth = $(tip).outerWidth(),
-                    tipHeight = $(tip).outerHeight();
 
                 // determine starting dimensions
                 switch (settings["direction"]) {
-                    case "up":
-                    case "down":
-                        var startDimensions = { "width" : tipWidth + "px" };
+                    case "up"    :
+                    case "down"  :
+                        startDimensions = { "width"  : tipWidth + "px" };
                     break;
     
-                    case "left":
-                    case "right":
-                        var startDimensions = { "height" : tipHeight + "px" };
+                    case "left"  :
+                    case "right" :
+                        startDimensions = { "height" : tipHeight + "px" };
                     break;
     
-                    case "none":
-                    default:
-                        var startDimensions = {
+                    case "none"  :
+                    default      :
+                        startDimensions = {
                             "width"  : tipWidth + "px",
                             "height" : tipHeight + "px"
                         };
                     break;
                 }
 
-                // set one of the sides to full
-                // width or height (depending on
-                // direction)
-                $(container).css(startDimensions);
-
-                // create an appropriate
-                // animation
+                // create an appropriate animation
                 switch (direction) {
                     case "left":
-                        var animation = {
+                        animation = $.extend(animation, {
                             "width" : tipWidth + "px",
                             "left"  : "-=" + tipWidth + "px"
-                        };
+                        });
                     break;
                     
                     case "right":
-                        var animation = { "width" : tipWidth + "px" };
+                        animation = $.extend(animation, { "width"  : tipWidth + "px" });
                     break;
                     
                     case "up":
-                        var animation = {
+                        animation = $.extend(animation, {
                             "height" : tipHeight + "px",
                             "top"    : "-=" + tipHeight + "px"
-                        };
+                        });
                     break;
                         
                     case "down":
-                        var animation = { "height" : tipHeight + "px" };
+                        animation = $.extend(animation, { "height" : tipHeight + "px" });
                     break;
                 }
 
+                // allow custom animations, this may a bad choice
                 animation = $.extend(animation, settings["showAnimation"]);
+
+                // set one of the sides to full width or height (depending on direction)
+                $(container).css(startDimensions);
+
+                debug("Calling onBeforeShow callback");
+
+                // call onShowStart as we insert into DOM
+                settings.onBeforeShow(container, container);
+
+                debug("Inserting into DOM"); 
+
+                // insert into DOM
+                $('body').append(container);
+
+                debug("Starting show animation");
 
                 // start animation with
                 // onShowEnd as callback
@@ -145,59 +175,68 @@
                     animation,
                     settings["duration"],
                     settings["showEasing"],
-                    settings.onAfterShow
+                    function() {
+                        debug("Calling onAfterShow callback");
+                        settings.onAfterShow.call(this, this);
+                    }
                 );
             },
 
             // on mouse out
             function() {
 
-                var container = $("#flexyTip"),
-                    tip = container.children(),
-                    tipWidth = $(tip).outerWidth(),
-                    tipHeight = $(tip).outerHeight();
+                debug("Starting onMouseOut");
 
-                settings.onBeforeHide(container);
+                var tipWidth       = $(tip).outerWidth(),
+                    tipHeight      = $(tip).outerHeight(),
+                    hideAnimation  = {};
 
+                debug("Calling onBeforeHide callback");
+
+                // call onBeforeHide event
+                settings.onBeforeHide.call(container, container);
+
+                // if the user gave a direction, do something with it
                 switch (settings["direction"]) {
-                    case "left":
-                        var hideAnimation = {
+                    case "left"  :
+                        $.extend(hideAnimation, {
                             "width" : "hide",
                             "left"  : "+=" + tipWidth + "px"
-                        };
+                        });
                     break;
                     
-                    case "right":
-                        var hideAnimation = { "width" : "hide" };
+                    case "right" :
+                        $.extend(hideAnimation, { "width" : "hide" });
                     break;
                     
-                    case "up":
-                        var hideAnimation = {
+                    case "up"    :
+                        $.extend(hideAnimation, {
                             "height" : "hide",
                             "top"    : "+=" + tipHeight + "px"
-                        };
+                        });
                     break;
                     
-                    case "down":
-                        var hideAnimation = { "height" : "hide" };
+                    case "down"  :
+                        $.extend(hideAnimation, { "height" : "hide" });
                     break;
-                    
-                    default:
-                        var hideAnimation = {};
-                    break;
+
+                    // no default
                 }
 
+                // allow for custom animation
                 hideAnimation = $.extend(hideAnimation, settings["hideAnimation"]);
 
-                // start animation with
-                // onHideEnd as callback
-                container.animate(
+                debug("Starting hide animation:");
+
+                // start animation with onHideEnd as callback
+                $(container).animate(
                     hideAnimation,
                     settings["duration"],
                     settings["hideEasing"],
                     function() {
-                        settings.onAfterHide();
-                        container.remove();
+                        debug("Calling onAfterHide callback");
+                        settings.onAfterHide.call(this, this);
+                        $(container).remove();
                     }
                 );
             });
